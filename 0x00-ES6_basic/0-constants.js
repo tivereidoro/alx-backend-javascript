5fceb10ea1aec3ccc5c0a0fffdbcb4c9a0c5092d
tivereidoro <tivereidoro@yahoo.com>
2024-03-05 21:01:26 +0000

    Task attempt 1

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+
+export function taskFirst() {
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  const combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
