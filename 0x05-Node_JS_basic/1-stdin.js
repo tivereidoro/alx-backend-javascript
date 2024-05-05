2043083c3be87b8220e53ff31f3ea0f7a7660d60
tivereidoro <tivereidoro@yahoo.com>
2024-05-05 23:33:55 +0100

    Fix: New line bug

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -8,5 +8,5 @@ process.stdin.on("readable", () => {
 });
 
 process.stdin.on("end", () => {
-  process.stdout.write("This important software is now closing");
+  process.stdout.write("This important software is now closing\n");
 });
