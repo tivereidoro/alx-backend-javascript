1cefa4baae0856009f169bb1f60e385c63b0fc50
tivereidoro <tivereidoro@yahoo.com>
2024-05-05 23:35:33 +0100

    Fix: New line bug

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,4 +1,4 @@
-process.stdout.write("Welcome to Holberton School, what is your name?");
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
 process.stdin.on("readable", () => {
   const name = process.stdin.read();
