ec05b512f8efd7cb539767ef3152fd5c7a27f642
tivereidoro <tivereidoro@yahoo.com>
2024-05-06 00:01:19 +0100

    Reading a csv file with nodeJs

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,1 +1,32 @@
-./
+const fs = require('fs');
+
+const countStudents = (path) => {
+  if (!fs.existsSync(path)) throw new Error('Cannot load the database');
+
+  const data = fs.readFileSync(path, 'utf8');
+
+  // Perform splits and turn row into an array
+  const students = data.split('\n')
+    .map((row) => row.split(','))
+    // Skip first row and onvert into objects
+    .filter((row) => row.length === 4 && row[0] !== 'firstname')
+    .map((row) => ({
+      firstName: row[0],
+      lastName: row[1],
+      age: row[2],
+      field: row[3].replace('\r', ''),
+    }));
+  // Creating CS students
+  const CS = students.filter((student) => student.field === 'CS')
+    .map((student) => student.firstName);
+  // Creating Software Engineering students
+  const SWE = students.filter((student) => student.field === 'SWE')
+    .map((student) => student.firstName);
+  // Print the length and convert each into a string
+  console.log(`Number of students: ${students.length}`);
+  console.log(`Number of students in CS: ${CS.length}. List: ${CS.join(', ')}`);
+  console.log(`Number of students in SWE: ${SWE.length}. List: ${SWE.join(', ')}`);
+};
+
+// Export as a module
+module.exports = countStudents;
